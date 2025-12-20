c71b8f1e964e4b46293e346f2f477385ea9c420a
Daffa Ilhami <mdaffailhami@gmail.com>
2025-12-20 08:00:19 +0800

    Add hero section

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	// import Navbar from './Navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<!-- <Navbar /> -->
+{@render children()}
---
+++ src/routes/(main)/sections/hero.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import heroBg from '$lib/assets/hero-bg.avif';
+	import logo2 from '$lib/assets/logo2.png';
+</script>
+
+<section
+	id="hero"
+	class="relative fl-h-[36rem/40rem] w-full bg-cover bg-center"
+	style:background-image="url({heroBg})"
+>
+	<div class="absolute inset-0 flex items-center justify-center bg-black/70">
+		<div class="flex max-w-7xl flex-col items-center gap-y-5 px-2">
+			<img src={logo2} alt="CryptoSharia Logo" class="fl-w-48/64" />
+			<div class="flex flex-col items-center text-center text-background text-shadow-lg">
+				<h1 class="fl-text-3xl/5xl font-semibold">CryptoSharia</h1>
+				<p class="fl-text-base/xl">Platform Edukasi dan Informasi Aset Kripto Syariah</p>
+			</div>
+		</div>
+	</div>
+</section>
