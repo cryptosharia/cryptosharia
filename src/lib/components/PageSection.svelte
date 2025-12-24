5e872cb15ffd22b0a1fd3bf7df92ce0fcb446199
Daffa Ilhami <mdaffailhami@gmail.com>
2025-12-24 18:13:15 +0800

    Style page scrollbar & forgot to spread the PageSection props

--- src/lib/components/PageSection.svelte
+++ src/lib/components/PageSection.svelte
@@ -2,9 +2,9 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { cn } from '$lib/utils';
 
-	let { children, class: className }: HTMLAttributes<HTMLDivElement> = $props();
+	let { children, class: className, ...rest }: HTMLAttributes<HTMLElement> = $props();
 </script>
 
-<section class={cn('mx-auto max-w-7xl fl-px-2.5/5', className)}>
+<section class={cn('mx-auto max-w-7xl fl-px-2.5/5', className)} {...rest}>
 	{@render children?.()}
 </section>
